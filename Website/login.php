ba83290d49842207e0b57e92c46df65729572b7c
0xWra1th <danielscragg@outlook.com>
2021-05-25 00:49:02 +0200

    New SQL

--- Website/login.php
+++ Website/login.php
@@ -1,14 +1,27 @@
 <?php
-$servername = "localhost";
-$username = "IEC";
-$password = "IEC_PASSWORD";
+$server= "localhost";
+$user = "IEC";
+$pass = "IEC_PASSWORD";
+$db = "IEC_DB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($server, $user, $pass, $db);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
