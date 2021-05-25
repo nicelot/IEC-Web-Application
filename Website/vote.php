8d4d29dd6c4bb4de60bf7777dd2fe7e36120abdf
0xWra1th <danielscragg@outlook.com>
2021-05-26 00:46:24 +0200

    Voting Complete

--- Website/vote.php
+++ Website/vote.php
@@ -8,50 +8,82 @@
     if(isset($_POST['Action'])){
         if($_POST['Action'] == 'create'){
             $candidate = $_POST['Candidate'];
-            $name = $_POST['Name'];
+            $accountEmail = $_SESSION['email'];
 
-            $query = "INSERT INTO `User` (Type, Name, Email) VALUES ('Voter', '$name', '$email')";
+            $query = "SELECT has_voted FROM Voter WHERE Email='$accountEmail';";
             $result = $conn->query($query);
+            $voted = $result->fetch_assoc()['has_voted'];
 
-            $query = "SELECT UserID FROM User WHERE Email='$email'";
-            $result = $conn->query($query);
-            $UserID = $result->fetch_assoc()['UserID'];
-
-            $query = "SELECT MunicipalityID FROM Municipality WHERE Name='$municipality'";
-            $result = $conn->query($query);
-            $muniID = $result->fetch_assoc()['MunicipalityID'];
-
-            $query = "INSERT INTO `Vote` (NationalID, Name, Surname, Password, Email, PhoneNr, MunicipalityId, UserId, has_voted) VALUES ('$id', '$name', '$surname', '$password', '$email', '$phone', '$muniID', '$UserID', 0);";
-            $result = $conn->query($query);
-            if($result){
-                $msg = "Vote cast!";
-            }else{
-                $msg = "Failed to cast vote!";
-                $query = "DELETE FROM User WHERE UserID = $UserID";
+            if($voted != 1){
+                $query = "SELECT Type FROM Candidate WHERE CandidateID='$candidate'";
                 $result = $conn->query($query);
+                $type = $result->fetch_assoc()['Type'];
+
+                $query = "INSERT INTO `Votes` (Type, CandidateID) VALUES ('$type', '$candidate');";
+                $result = $conn->query($query);
+
+                $query = "UPDATE Voter SET has_voted=1 WHERE Email='$accountEmail';";
+                $result = $conn->query($query);
+
+                if($result){
+                    $msg = "Vote cast!";
+                }else{
+                    $msg = "Failed to cast vote!";
+                }
+            }else{
+                $msg = "You have already voted!";
             }
         }
     }
 ?>
 
 <html>
-    <head>
-        <title>Vote!</title>
-    </head>
-    <body>
-        <center>
-            <div style="background-color: gray; width: 225px; height: 250px;margin-top:25px" id="Register Voter">
-                <h2 style="padding-top: 25px">Vote!</h2>
-                <form method="POST" action="vote.php" enctype="multipart/form-data">
-                    <label id="Candidate-Label">Candidate:</label><br>
-                    <input required name="Candidate" type="text"><br>
-                    <label id="Municipality-Label">Municipality Type: </label><br>
-                    <input required name="Type" type="text"><br><br>
-                    <input type="submit" value="Register">
-                    <?php echo "<h3 style=\"color: red\">$msg</h3>" ?>
-                    <input hidden name="Action" value="create">
-                </form>
-            </div>
-        </center>
-    </body>
+<head>
+    <title>Vote!</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="votercss.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+<body>
+<nav class="navtop">
+    <div>
+        <h1>Electoral Commission of South Africa</h1>
+        <a href="results.php"><i class="fas fa-poll-h"></i>Results</a>
+    </div>
+</nav>
+<center>
+    <div id="Register Voter">
+        <h2>Vote!</h2>
+        <form method="POST" class="formcan" action="vote.php" enctype="multipart/form-data">
+            <label class ="formlab" id="Candidate-Label">Candidate:</label><br>
+            <input required class="canlab" name="Candidate" type="text"><br>
+            <input id="vote" class="canlab" type="submit" value="Register">
+            <?php echo "<h3 style=\"color: red\">$msg</h3>" ?>
+            <input hidden name="Action" value="create">
+        </form>
+    </div>
+    <div id="Register Voter">
+        <h2>Candidates</h2>
+        <div style="margin: 20px;background-color: #3f69a8; border-radius: 5px; padding: 15px 0px 15px 0px;width: 500px">
+            <?php
+                require('connect.php');
+
+                $sql = "SELECT CandidateID, Name, Surname FROM Candidate";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                  echo "<table><tr><th>ID</th><th>Name</th></tr>";
+                  while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>".$row["CandidateID"]."    -    </td><td>".$row["Name"]." ".$row["Surname"]."</td></tr>";
+                  }
+                  echo "</table>";
+                } else {
+                  echo "0 results";
+                }
+                $conn->close();
+            ?>
+        </div>
+    </div>
+</center>
+</body>
 </html>
